716d80b31d83e31bfbe440976893a29ed41f0081
BuildTools <unconfigured@null.spigotmc.org>
2017-10-20 13:06:18 +0700

    added something

--- src/main/java/Main/Bot.java
+++ src/main/java/Main/Bot.java
@@ -6,7 +6,7 @@ import sx.blah.discord.api.IDiscordClient;
 import utils.PropertiesUtil;
 
 public class Bot{
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final String CREATOR_ID = "242526051442491392";
 	
 	public static IDiscordClient client;
